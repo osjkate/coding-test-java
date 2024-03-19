0470ecca763910ff38c12570da6dc97478bc03a0
osjkate <osjkate@sookmyung.ac.kr>
2024-03-19 18:37:26 +0900

    [코테스터디] DFS & BFS

---
+++ src/boj/study/week6/week6_3_p1260.java
@@ -0,0 +1,70 @@
+package boj.study.week6;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class week6_3_p1260 {
+    static boolean[] visited_arr;
+    static int[][] tree;
+    static int count;
+    static int N;
+    static Queue<Integer> que = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        tree = new int[1001][1001];
+        visited_arr = new boolean[1001];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            tree[x][y] = tree[y][x] = 1;
+        }
+
+        DFS(V);
+        System.out.println();
+        visited_arr = new boolean[1001];
+        BFS(V);
+
+    }
+    public static void DFS(int start) {
+        visited_arr[start] = true;
+        System.out.print(start + " ");
+
+        if (count == N) {
+            return;
+        }
+        count++;
+
+        for (int i = 1; i <= N; i++) {
+            if (tree[start][i] == 1 && !visited_arr[i]) {
+                DFS(i);
+            }
+        }
+    }
+
+    public static void BFS(int start) {
+        que.offer(start);
+        visited_arr[start] = true;
+        System.out.print(start + " ");
+
+        while (!que.isEmpty()) {
+            start = que.poll();
+
+            for (int i = 1; i <= N; i++) {
+                if (tree[start][i] == 1 && !visited_arr[i]) {
+                    que.offer(i);
+                    visited_arr[i] = true;
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+}
